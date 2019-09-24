2a6b87d6156544b13326bd0730a39a073574ae8a
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-09-24 15:51:55 -0400

    added xcode project

---
+++ SwiftDemo/SwiftDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDemo
+//
+//  Created by Maitree Bain on 9/24/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Maitree")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
